c9b1ec98fb5b575e3f4fdd864bbeac506b8e4229
HughesPHS <Hughesphs@gmail.com>
2017-04-13 21:43:12 -0400

    Update hangman.py

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -17,7 +17,7 @@ def _get_random_word(word_list):
     Returns a random word from the word_list to use as the answer word.
     :param word_list: The list of possible answer words
     """
-    pass
+    return random.choice(word_list) #in word_list
 
 
 def _mask_word(word):
@@ -27,7 +27,10 @@ def _mask_word(word):
     :param word: The answer word for the current game
     Example: word - 'cat'     masked word - '***'
     """
-    pass
+    maskedWord = ""
+    for i in range(len(word)):
+        maskedWord += '*'
+    return maskedWord
 
 
 def _guess_is_valid(guessed_letter, previous_guesses):
@@ -38,15 +41,22 @@ def _guess_is_valid(guessed_letter, previous_guesses):
     :param previous_guesses: A string of all the letters previously guessed
     Returns True if given guess is valid, False otherwise.
     """
-    pass
-
+    if len(guessed_letter) > 1:
+        return False
+    for i in previous_guesses:
+        if i == guessed_letter:
+            return False
+    return guessed_letter.isalpha()
 
 def _check_lose(remaining_misses):
     """
     Returns True if remaining guesses is equal to 0 and false otherwise.
     :param remaining_misses: How many misses are left before user loses
     """
-    pass
+    if remaining_misses <= 0:
+        return True
+    else:
+        return False
 
 
 def _check_win(answer_word, masked_word):
@@ -59,7 +69,10 @@ def _check_win(answer_word, masked_word):
     :param masked_word: The answer word masked with '*' characters for letters
                         that haven't been guessed
     """
-    pass
+    if answer_word == masked_word:
+        return True
+    else:
+        return False
 
 
 def _check_game_over(answer_word, masked_word, remaining_misses):
@@ -71,9 +84,17 @@ def _check_game_over(answer_word, masked_word, remaining_misses):
                         that haven't been guessed
     :param remaining_misses: How many misses are left before user loses
     """
-    pass
-
-
+    if _check_win(answer_word, masked_word) == True or _check_lose(remaining_misses) == True:
+        return True
+    # elif _check_lose(remaining_misses) == True:
+    #     return True
+    else:
+        return False
+"""
+E     File "/home/ubuntu/workspace/hangman/hangman.py", line 110                                  
+E       answer = 'answer_word': _get_random_word(word_list),                                      
+E                             ^                                
+"""
 # Public interface
 def start_new_game(word_list, answer_word=None):
     """
@@ -86,7 +107,17 @@ def start_new_game(word_list, answer_word=None):
     previous_guesses - The previous valid letter guesses the user has inputted
     remaining_misses - how many misses the user has left. Start with 5.
     """
-    pass
+    answer = {
+        
+    }
+    if answer_word == None:
+        answer['answer_word'] = _get_random_word(word_list)
+    else:
+        answer['answer_word'] = answer_word
+    answer['masked_word'] = _mask_word(answer['answer_word'])
+    answer['previous_guesses'] = ""
+    answer['remaining_misses'] = 5
+    return answer
 
 
 def guess_letter(game, letter):
@@ -104,9 +135,19 @@ def guess_letter(game, letter):
     :param game: The dictionary storing current game information
     :param letter: The letter that is being guessed
     """
-    pass
-
-
+    isCorrect = False
+    game['previous_guesses'] += letter
+    
+    for index, character in enumerate(game['answer_word'] ): # https://docs.python.org/2/library/functions.html#enumerate
+        if character == letter:
+            isCorrect = True
+            tempList = list(game['masked_word'])
+            tempList[index] = letter
+            game['masked_word'] = ''.join(tempList) # ['a','b','c'] ''.join -> abc, '-'.join -> a-b-c
+            
+    if isCorrect == False:
+        game['remaining_misses'] -= 1
+        
 def user_input_guess(game):
     """
     Repeats user input guesses until game is over. If guess is invalid,
